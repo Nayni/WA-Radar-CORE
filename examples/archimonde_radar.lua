439ed51f6c0b65e626d6f05beae0260f58b20275
Rutger Hendrickx <rutgerhendrickx@gmail.com>
2016-07-17 22:16:21 +0200

    clean up archimonde example, add raidmarkers

--- examples/archimonde_radar.lua
+++ examples/archimonde_radar.lua
@@ -12,22 +12,30 @@ local CUSTOM_TRIGGER = function(event, ...)
 
       aura_env.lines = aura_env.lines or {}
       aura_env.shackles = aura_env.shackles or {}
+      aura_env.shackleCount = aura_env.shackleCount or 0
+
+      local SHACKLE_RADIUS = 25.5
 
       if subevent == "SPELL_AURA_APPLIED" and spellId == 184964 then
             core:Enable()
+            aura_env.shackleCount = aura_env.shackleCount + 1
 
-            local radius = 25.5
             local key = "SHACKLE_" .. destGUID
+            local text = "Shackle " .. aura_env.shackleCount
             -- convert the destGUIDs current position into a static point and place it on the radar
-            core:Static(key, destGUID)
+            -- we pass the shackle count as a number, it'll make the static point look like a raid marker on the radar
+            -- 1 = star, 2 = circle, 3 = diamond
+            core:Static(key, destGUID, aura_env.shackleCount)
             -- now we place a disk on the static point, indicates the shackle radius
-            core:Disk(key, radius)
+            core:Disk(key, SHACKLE_RADIUS, text)
             -- save the disk ref, we need it for later
             aura_env.shackles[key] = disk
       end
 
       if subevent == "SPELL_AURA_REMOVED" and spellId == 184964 then
             core:Enable()
+            aura_env.shackleCount = aura_env.shackleCount - 1
+
             local key = "SHACKLE_" .. destGUID
             -- look up the disk
             local disk = aura_env.shackles[key]
@@ -51,8 +59,9 @@ local CUSTOM_TRIGGER = function(event, ...)
             if focused and wrought then
                   -- we can give WA_RADAR_CORE GUIDs and names, it will deal with it for you
                   -- a width of 4 is perfect for archimonde focused chaos beams
+                  --
                   -- EXTEND will extend the line on the destination, we want the line to be extended on the focused target
-                  -- so we pass wrought as the source, and focused as the destination
+                  -- so we pass wrought as the source, and focused as the destination and tell the radar to extend
                   local line = core:Connect(wrought, focused, 4, EXTEND)
                   -- keep the line ref, need it for later
                   aura_env.lines[focused] = line
@@ -79,6 +88,7 @@ local CUSTOM_TRIGGER = function(event, ...)
             C_Timer.After(5, function()
                   aura_env.shackles = {}
                   aura_env.lines = {}
+                  aura_env.shackleCount = 0
                   core:Disable()
             end)
       end
@@ -87,6 +97,7 @@ local CUSTOM_TRIGGER = function(event, ...)
       if event == "ENCOUNTER_END" and encounterId == 1799 then
             aura_env.shackles = {}
             aura_env.lines = {}
+            aura_env.shackleCount = 0
             core:Disable()
       end
 
